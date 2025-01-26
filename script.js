b4e132eefde988b413668c426c41588b463b7417
Pabitra-33 <kumarpabitra345@gmail.com>
2025-01-27 00:11:38 +0530

    Second commit

--- script.js
+++ script.js
@@ -98,8 +98,3 @@ keys.addEventListener('click', (event) => {
     inputDigit(target.value);
     updateDisplay();
 });
-
-//Explanation
-//HTML: This sets up the structure of the calculator, including the display screen and the buttons.
-//CSS: This styles the calculator to make it look visually appealing.
-//JavaScript: This handles the logic for the calculator's operations, including handling digit input, operators, and updating the display.
